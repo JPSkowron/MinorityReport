8723958d13e42d3febc1cd49e994a2f4c5714b90
JP Skowron <jpskowron@JP-Skowrons-MacBook-Pro.local>
2015-01-08 12:00:33 -0600

    Label move to center

--- MinorityReport/RootViewController.m
+++ MinorityReport/RootViewController.m
@@ -11,6 +11,7 @@
 @interface RootViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *theFutureLabel;
 @property (weak, nonatomic) IBOutlet UILabel *thePreCpgsLabel;
+@property CGPoint originalCenter;
 
 @end
 
@@ -19,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationController.navigationBar.barTintColor = [UIColor greenColor];
+    self.originalCenter = self.theFutureLabel.center;
 
 
 }
@@ -31,6 +33,11 @@
     if (CGRectContainsPoint(self.thePreCpgsLabel.frame, point)) {
         self.theFutureLabel.text = @"A ficticious and incriminating future";
         [self.theFutureLabel sizeToFit];
+    }
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        [UIView animateWithDuration: 1.f animations:^{
+            self.theFutureLabel.center = self.originalCenter;
+        }];
     }
 }
 
